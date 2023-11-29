ee7bda3ecbde12a77c3fcca0af50c80f48079160
TOYINNO <toyinnomons@gmail.com>
2023-11-29 17:15:04 +0100

    fix modify changes

--- src/app/course/page.jsx
+++ src/app/course/page.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
+"use client"
 import Link from 'next/link'
+import { useState } from 'react'
+
 
 export const books = [
   {
@@ -50,7 +52,6 @@ export const books = [
     type: 'Thriller',
     summary: 'A psychological novel that explores the complexities of motherhood and the impact of intergenerational trauma.'
   },
-  // Additional entries with years, type, and summary
   {
     id: '007',
     title: 'The Silent Patient',
@@ -76,14 +77,6 @@ export const books = [
     summary: 'A magical tale of a mysterious circus that appears only at night, showcasing fantastical wonders and illusions.'
   },
   {
-    id: '010',
-    title: 'Sapiens: A Brief History of Humankind',
-    author: 'Yuval Noah Harari',
-    year: 2014,
-    type: 'NonFiction',
-    summary: 'A thought-provoking exploration of the history of Homo sapiens, from the emergence of our species to modern times.'
-  },
-  {
     id: '011',
     title: 'The Great Gatsby',
     author: 'F. Scott Fitzgerald',
@@ -98,6 +91,121 @@ export const books = [
     year: 2017,
     type: 'Fiction',
     summary: 'A heartwarming and quirky novel about an eccentric woman\'s journey towards self-discovery and connection with others.'
+  },
+  {
+    id: '014',
+    title: 'The Da Vinci Code',
+    author: 'Dan Brown',
+    year: 2003,
+    type: 'Thriller',
+    summary: 'Dan Brown\'s "The Da Vinci Code" is a pulse-pounding thriller that follows symbologist Robert Langdon as he is drawn into a mysterious web of secrets, codes, and ancient symbols. The discovery of a murder victim in the Louvre Museum sets off a chain of events that lead Langdon and cryptologist Sophie Neveu on a quest to unravel a hidden truth guarded for centuries. As they navigate through cryptic messages and historical mysteries, the duo must stay one step ahead of a relentless adversary. With twists and turns at every corner, "The Da Vinci Code" keeps readers on the edge of their seats in this high-stakes race against time.'
+
+  },
+  {
+    id: '015',
+    title: 'Harry Potter and the Sorcerer\'s Stone',
+    author: 'J.K. Rowling',
+    year: 1997,
+    type: 'Fantasy',
+    summary: 'J.K. Rowling\'s "Harry Potter and the Sorcerer\'s Stone" introduces readers to the magical world of Hogwarts and the young wizard, Harry Potter. As Harry discovers his wizarding abilities and befriends Ron Weasley and Hermione Granger, they embark on a journey to uncover the mystery behind the Sorcerer\'s Stone. With its enchanting characters, magical creatures, and rich world-building, this fantasy novel captivates readers of all ages. Rowling weaves a tale of friendship, courage, and the triumph of good over evil, setting the stage for the epic adventures that follow in the rest of the series.'
+  },
+  {
+    id: '016',
+    title: 'The Glass Castle',
+    author: 'Jeannette Walls',
+    year: 2005,
+    type: 'Memoir',
+    summary: 'Jeannette Walls\'s "The Glass Castle" is a compelling memoir that unveils the author\'s tumultuous and unconventional childhood. Raised by eccentric parents who held unconventional views on life, Walls recounts her experiences with humor and resilience. From their nomadic lifestyle to the challenges of poverty and her parents\' unorthodox approach to parenting, Walls provides a candid and introspective look into her past. "The Glass Castle" is a poignant exploration of family dynamics, resilience, and the enduring impact of a unique and challenging upbringing.'
+  },
+  {
+    id: '017',
+    title: 'To Kill a Mockingbird',
+    author: 'Harper Lee',
+    year: 1960,
+    type: 'Classic',
+    summary: 'Harper Lee\'s "To Kill a Mockingbird" is a timeless classic that explores themes of racial injustice, moral growth, and compassion in the American South during the 1930s. Through the eyes of Scout Finch, the novel follows her father, attorney Atticus Finch, as he defends a black man wrongly accused of raping a white woman. Lee masterfully addresses issues of prejudice, empathy, and the complexities of human nature. "To Kill a Mockingbird" is a powerful narrative that resonates with readers for its profound insights into the human condition and its enduring relevance in today\'s society.'
+  },
+  {
+    id: '018',
+    title: 'The Immortal Life of Henrietta Lacks',
+    author: 'Rebecca Skloot',
+    year: 2010,
+    type: 'NonFiction',
+    summary: 'Rebecca Skloot\'s "The Immortal Life of Henrietta Lacks" is a riveting work of non-fiction that delves into the story of Henrietta Lacks, whose cells were unknowingly used for scientific research and became instrumental in medical advancements. Skloot intricately weaves together the scientific journey of HeLa cells with the personal history of the Lacks family. The book raises thought-provoking ethical questions about medical consent, the commercialization of human tissues, and the impact of scientific progress on individuals. "The Immortal Life of Henrietta Lacks" is a captivating exploration of the intersection between science, ethics, and the human experience.'
+
+  },
+  {
+    id: '019',
+    title: 'Gone Girl',
+    author: 'Gillian Flynn',
+    year: 2012,
+    type: 'Thriller',
+    summary: 'Gillian Flynn\'s "Gone Girl" is a psychological thriller that twists and turns with every page. When Amy Dunne goes missing on her fifth wedding anniversary, suspicion falls on her husband Nick. As the media frenzy intensifies, secrets and lies unravel, leading to a gripping narrative that challenges perceptions and keeps readers guessing until the shocking conclusion. Flynn skillfully explores the complexities of marriage, deception, and the public perception of crime in this dark and compelling thriller.'
+  },
+  {
+    id: '020',
+    title: 'The Lord of the Rings',
+    author: 'J.R.R. Tolkien',
+    year: 1954,
+    type: 'Fantasy',
+    summary: `J.R.R. Tolkien's "The Lord of the Rings" is an epic fantasy trilogy that transports readers to the enchanting world of Middle-earth. The quest to destroy the One Ring and thwart the dark lord Sauron unfolds across a vast landscape inhabited by diverse races, cultures, and magical creatures. Tolkien's intricate world-building, rich mythology, and memorable characters make this fantasy classic a masterpiece of the genre. With themes of friendship, sacrifice, and the battle between good and evil, "The Lord of the Rings" has captivated readers for generations.`
+
+  },
+  {
+    id: '021',
+    title: 'Bossypants',
+    author: 'Tina Fey',
+    year: 2011,
+    type: 'Memoir',
+    summary: `Tina Fey's "Bossypants" is a hilarious and candid memoir that offers a glimpse into the life and career of the acclaimed comedian and writer. From her early days in improv comedy to becoming the first female head writer of "Saturday Night Live", Fey shares anecdotes and insights with her trademark wit. "Bossypants" is not only a memoir but also a celebration of female empowerment and resilience in the male-dominated world of comedy. Fey's storytelling prowess and humor make this memoir an entertaining and inspiring read.`
+  },
+  {
+    id: '022',
+    title: '1984',
+    author: 'George Orwell',
+    year: 1949,
+    type: 'Classic',
+    summary: 'A dystopian novel that explores themes of totalitarianism, surveillance, and the manipulation of truth.'
+  },
+  {
+    id: '023',
+    title: 'The Power of Habit',
+    author: 'Charles Duhigg',
+    year: 2012,
+    type: 'NonFiction',
+    summary: 'An exploration of the science behind habits, why they exist, and how they can be changed to transform our lives.'
+  },
+  {
+    id: '024',
+    title: 'The Silent Patient',
+    author: 'Alex Michaelides',
+    year: 2019,
+    type: 'Thriller',
+    summary: 'A psychological thriller about a woman\'s sudden violence against her husband and her subsequent silence.'
+  },
+  {
+    id: '025',
+    title: 'The Name of the Wind',
+    author: 'Patrick Rothfuss',
+    year: 2007,
+    type: 'Fantasy',
+    summary: 'The first book in the Kingkiller Chronicle series, following the life and adventures of the gifted young musician Kvothe.'
+  },
+  {
+    id: '026',
+    title: 'Eat, Pray, Love',
+    author: 'Elizabeth Gilbert',
+    year: 2006,
+    type: 'Memoir',
+    summary: 'A memoir chronicling the author\'s journey of self-discovery through Italy, India, and Indonesia after a difficult divorce.'
+  },
+  {
+    id: '027',
+    title: 'Moby-Dick',
+    author: 'Herman Melville',
+    year: 1851,
+    type: 'Classic',
+    summary: 'A classic novel exploring the obsession of Captain Ahab with the white whale Moby-Dick and the themes of fate and revenge.'
   }
 ]
 
@@ -111,7 +219,32 @@ const bookCard = books.map(book=>(
 ))
 
 export default function Course() {
+
+  const [searchBook, setSearchBook] = useState('')
+  const filteredBook = books.filter(book => book.title.toLowerCase().includes(searchBook.toLowerCase()))
+
+  function onSubmit(e){
+    e.preventDefault()
+    setSearchBook(e.target.search.value)
+  }
+
+  const bookCard = filteredBook.map(book =>(
+    <section key={book.id} className='p-[4rem] shadow-2xl'>
+      <Link href={`/books/${book.title.split(' ').join('-')}`} title={`${book.title} by ${book.author}`}>
+        <h2>{book.title}</h2>
+        <p>{book.author}</p>
+      </Link>
+    </section>
+  ))
   return (
-    <div className='grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4'>{bookCard}</div>
+    <div>
+      <form className='flex items-center justify-center' onSubmit={onSubmit}>
+        <input type="text" name="search" id="search" placeholder='search for book or Author' className='w-3/5 outline-none rounded-[40px] border-2 border-gray-700 border-solid p-4 my-5 m-auto'
+        defaultValue={searchBook} />
+      </form>
+      <div className='grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4'>
+      {bookCard}
+      </div>
+      </div>
   )
 }
